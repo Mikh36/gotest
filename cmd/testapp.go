b189903a181ec3c572fa5a08cdc2d7d3a4e19781
Murzin Mikhail <mikhai.murzin@x5.ru>
2024-01-16 13:20:00 +0300

    Add API handle

--- cmd/testapp.go
+++ cmd/testapp.go
@@ -15,6 +15,10 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
 }
 
+func Api(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "It's API!\n")
+}
+
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
@@ -22,20 +26,21 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 func main() {
 	log.Println("Read config")
 	var cfg map[string]string = map[string]string{"port": "8080"} // Обьявлем и иницилизируем переменную с дефолтным портом.
-	cleanenv.ReadConfig("config.yaml", &cfg)                      // Если есть файл с конфином берем значение порта оттуда.
+	cleanenv.ReadConfig("config.yaml", &cfg)                      // Если есть файл с конфигом берем значение порта оттуда.
 
 	for _, e := range os.Environ() {
 		pair := strings.SplitN(e, "=", 2)
 		if pair[0] == "APP_PORT" {
-			log.Printf(fmt.Sprintf("Set the application port from environment variables in the value: %s", pair[1]))
-			cfg["port"] = pair[1] // Если есть переменная окружения берем порт их окружения
+			log.Printf("Set the application port from environment variables in the value: %s", pair[1])
+			cfg["port"] = pair[1] // Если есть переменная окружения берем порт из окружения.
 		}
 	}
 
 	router := httprouter.New()
 	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
+	router.GET("/api/", Api)
+	router.GET("/api/hello/:name", Hello)
 
-	log.Printf(fmt.Sprintf("We use port: %s", cfg["port"]), router)
+	log.Printf("We use port: %s", cfg["port"])
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", cfg["port"]), router))
 }
